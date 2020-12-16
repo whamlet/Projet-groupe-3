a047979728f64e47a0a02331dadf28a5454425ad
whamlet <75739561+whamlet@users.noreply.github.com>
2020-12-16 22:30:05 +0100

    Update Temps_fixation.R

--- Temps_fixation.R
+++ Temps_fixation.R
@@ -1,4 +1,7 @@
 one.simu<- function(N, p){
+  population<- (sample(c(1,0), replace = TRUE, size =2*N, prob =c(p, 1-p)))
+  a <- sum(population == "1")
+  p <- ((N*2) - a)/(2*N)
   f1 <- -N^2*((1 - p)*log10(1 - p) + p*log10(p))
   return(f1)
 }
